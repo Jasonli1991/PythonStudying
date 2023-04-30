bf4cdcc59bd69e71ce11502f03a160e3ff7ef0f4
Jasonli1991 <chih.chung.li.1991@gmail.com>
2023-04-30 21:58:35 +0800

    04302158

---
+++ lab_codes/mod08/whilestmt1.py
@@ -0,0 +1,10 @@
+# while stmt
+m = int(input('Input factorial: '))
+r = n = 1
+
+while n <= m:
+    r *= n    # r = r * n
+    n += 1    # n = n + 1
+
+print(str(m) + '! =', r)
+print(f'{m}! = {r}')
